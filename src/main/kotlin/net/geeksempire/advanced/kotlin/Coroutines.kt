f26be1b2105dfff5d25578097cc08e1fa51844d1
GeeksEmpireNet <Support@GeeksEmpire.net>
2019-12-18 20:12:19 -0900

    Initial

--- src/main/kotlin/net/geeksempire/advanced/kotlin/Coroutines.kt
+++ src/main/kotlin/net/geeksempire/advanced/kotlin/Coroutines.kt
@@ -5,17 +5,43 @@ import kotlinx.coroutines.*
 class Coroutines {
 
     init {
-        aCoroutineTask()
+        finalizeResourcesInCoroutine()
     }
 
     fun aCoroutineTask() = runBlocking<Unit> {
         var aCoroutineTask: Job? = GlobalScope.launch {
             delay(1000)
 
-            println("11th Sentence")
+            println("First Sentence")
         }
 
-        println("12th Sentence") // main thread continues while coroutine is delayed
+        println("Second Sentence") // main thread continues while coroutine is delayed
         aCoroutineTask?.join()
+    }
+
+    fun repeatCoroutine() = runBlocking<Unit> {
+        val job = launch {
+            repeat(10) { i ->
+                println("job: I'm sleeping $i ...")
+                delay(777)
+            }
+        }
+    }
+
+    fun finalizeResourcesInCoroutine() = runBlocking {
+        val job = launch {
+            try {
+                repeat(1000) { i ->
+                    println("job: I'm sleeping $i ...")
+                    delay(500L)
+                }
+            } finally {
+                println("job: I'm running finally")
+            }
+        }
+        delay(1300L) // delay a bit
+        println("main: I'm tired of waiting!")
+        job.cancelAndJoin() // cancels the job and waits for its completion
+        println("main: Now I can quit.")
     }
 }
