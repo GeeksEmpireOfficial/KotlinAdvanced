d7895a3307090a74b5476efe22b806dd6964a0e0
GeeksEmpireNet <Support@GeeksEmpire.net>
2019-12-19 15:01:23 -0900

    Initial

--- src/main/kotlin/net/geeksempire/advanced/kotlin/Coroutines.kt
+++ src/main/kotlin/net/geeksempire/advanced/kotlin/Coroutines.kt
@@ -1,11 +1,12 @@
 package net.geeksempire.advanced.kotlin
 
 import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
 class Coroutines {
 
     init {
-        finalizeResourcesInCoroutine()
+        lazyCoroutine()
     }
 
     fun aCoroutineTask() = runBlocking<Unit> {
@@ -43,5 +44,51 @@ class Coroutines {
         println("main: I'm tired of waiting!")
         job.cancelAndJoin() // cancels the job and waits for its completion
         println("main: Now I can quit.")
+    }
+
+    fun sequentialCoroutine() = runBlocking {
+        val time = measureTimeMillis {
+            val oneProcess = doSomethingUsefulOne()
+            val twoProcess = doSomethingUsefulTwo()
+
+            println("Answer ::: ${oneProcess + twoProcess}")
+        }
+        println("Completed in $time ms")
+    }
+
+    fun concurrentCoroutines() = runBlocking {
+        val time = measureTimeMillis {
+            val one = async { doSomethingUsefulOne() }
+            val two = async { doSomethingUsefulTwo() }
+
+            println("Answer ::: ${one.await()}")
+            println("Answer ::: ${two.await()}")
+            println("Answer ::: ${one.await() + two.await()}")
+        }
+        println("Completed in $time ms")
+    }
+
+    fun lazyCoroutine() = runBlocking {
+        val time = measureTimeMillis {
+            println("Answer ::: ${concurrentCoroutine()}")
+        }
+        println("Completed in $time ms")
+    }
+
+    suspend fun concurrentCoroutine(): Int = coroutineScope {
+        val one = async { doSomethingUsefulOne() }
+        val two = async { doSomethingUsefulTwo() }
+
+        one.await() + two.await()
+    }
+    suspend fun doSomethingUsefulOne(): Int {
+        delay(1000L) // pretend we are doing something useful here
+
+        return 1
+    }
+    suspend fun doSomethingUsefulTwo(): Int {
+        delay(3000L) // pretend we are doing something useful here, too
+
+        return 2
     }
 }
