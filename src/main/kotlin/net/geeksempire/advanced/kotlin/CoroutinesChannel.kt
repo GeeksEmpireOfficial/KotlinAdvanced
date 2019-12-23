cc49c6e78059b45b66f3b8f30f5115c04f3eb279
GeeksEmpireNet <Support@GeeksEmpire.net>
2019-12-22 19:16:20 -0900

    Initial

--- src/main/kotlin/net/geeksempire/advanced/kotlin/CoroutinesChannel.kt
+++ src/main/kotlin/net/geeksempire/advanced/kotlin/CoroutinesChannel.kt
@@ -1,18 +1,28 @@
 package net.geeksempire.advanced.kotlin
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.cancelChildren
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.ReceiveChannel
-import kotlinx.coroutines.channels.consumeEach
-import kotlinx.coroutines.channels.produce
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.*
 
 class CoroutinesChannel {
 
     init {
-        coroutinesPipeline()
+        tickingProducer()
+    }
+
+    fun bufferChannel() = runBlocking {
+        val channel = Channel<Int>(2) // create buffered channel
+
+        val sender = launch { // launch sender coroutine
+            repeat(10) {
+                println("Sending $it") // print before sending each element
+
+                channel.send(it) // will suspend when buffer is full
+            }
+        }
+
+        // don't receive anything... just wait....
+        delay(1000)
+        sender.cancel() // cancel sender coroutine
     }
 
     //One Coroutine produce stream of data & other coroutines are using it.
@@ -77,5 +87,29 @@ class CoroutinesChannel {
         }
 
         println("Done!")
+    }
+
+    fun tickingProducer() = runBlocking<Unit> {
+        val tickerChannel = ticker(delayMillis = 100, initialDelayMillis = 0) // create ticker channel
+        var nextElement = withTimeoutOrNull(1) { tickerChannel.receive() }
+        println("Initial element is available immediately: $nextElement") // initial delay hasn't passed yet
+
+        nextElement = withTimeoutOrNull(50) { tickerChannel.receive() } // all subsequent elements has 100ms delay
+        println("Next element is not ready in 50 ms: $nextElement")
+
+        nextElement = withTimeoutOrNull(60) { tickerChannel.receive() }
+        println("Next element is ready in 100 ms: $nextElement")
+
+        // Emulate large consumption delays
+        println("Consumer pauses for 150ms")
+        delay(150)
+        // Next element is available immediately
+        nextElement = withTimeoutOrNull(1) { tickerChannel.receive() }
+        println("Next element is available immediately after large consumer delay: $nextElement")
+        // Note that the pause between `receive` calls is taken into account and next element arrives faster
+        nextElement = withTimeoutOrNull(60) { tickerChannel.receive() }
+        println("Next element is ready in 50ms after consumer pause in 150ms: $nextElement")
+
+        tickerChannel.cancel() // indicate that no more elements are needed
     }
 }
