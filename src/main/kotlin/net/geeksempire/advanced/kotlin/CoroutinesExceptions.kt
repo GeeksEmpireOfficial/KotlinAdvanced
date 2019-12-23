08e882ed2bc253eecfd4cc56c54195acb87cca3c
GeeksEmpireNet <Support@GeeksEmpire.net>
2019-12-23 14:07:58 -0900

    Initial

---
+++ src/main/kotlin/net/geeksempire/advanced/kotlin/CoroutinesExceptions.kt
@@ -0,0 +1,67 @@
+package net.geeksempire.advanced.kotlin
+
+import kotlinx.coroutines.*
+
+class CoroutinesExceptions {
+
+    init {
+        supervisedJobException()
+    }
+
+    fun exceptionHandler() = runBlocking {
+        val handler = CoroutineExceptionHandler { _, exception ->
+            println("Caught $exception")
+        }
+
+        val job = GlobalScope.launch(handler) {
+            launch { // the first child
+                try {
+                    delay(Long.MAX_VALUE)
+                } finally {
+                    withContext(NonCancellable) {
+                        println("Children are cancelled, but exception is not handled until all children terminate")
+                        delay(100)
+                        println("The first child finished its non cancellable block")
+                    }
+                }
+            }
+            launch { // the second child
+                delay(10)
+                println("Second child throws an exception")
+                throw ArithmeticException()
+            }
+        }
+        job.join()
+    }
+
+    fun supervisedJobException() = runBlocking {
+        val supervisor = SupervisorJob()
+        with(CoroutineScope(coroutineContext + supervisor)) {
+            // launch the first child -- its exception is ignored for this example (don't do this in practice!)
+            val firstChild = launch(CoroutineExceptionHandler { _, _ ->  }) {
+                println("First child is failing")
+                throw AssertionError("First child is cancelled")
+            }
+
+            // launch the second child
+            val secondChild = launch {
+                firstChild.join()
+                // Cancellation of the first child is not propagated to the second child
+                println("First child is cancelled: ${firstChild.isCancelled}, but second one is still active")
+                try {
+                    delay(2000)
+                } finally {
+                    // But cancellation of the supervisor is propagated
+                    println("Second child is cancelled because supervisor is cancelled")
+                }
+            }
+            // wait until the first child fails & completes
+            firstChild.join()
+
+            println("Cancelling supervisor")
+            supervisor.cancel()
+
+            secondChild.join()
+        }
+    }
+}
