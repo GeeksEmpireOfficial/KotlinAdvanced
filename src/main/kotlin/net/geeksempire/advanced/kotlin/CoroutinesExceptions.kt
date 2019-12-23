67af2c3caf0cb19d7ef866077b665312bb84aca9
GeeksEmpireNet <Support@GeeksEmpire.net>
2019-12-23 14:09:59 -0900

    Initial

--- src/main/kotlin/net/geeksempire/advanced/kotlin/CoroutinesExceptions.kt
+++ src/main/kotlin/net/geeksempire/advanced/kotlin/CoroutinesExceptions.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.*
 class CoroutinesExceptions {
 
     init {
-        supervisedJobException()
+        supervisedScopeException()
     }
 
     fun exceptionHandler() = runBlocking {
@@ -63,5 +63,19 @@ class CoroutinesExceptions {
 
             secondChild.join()
         }
+    }
+
+    fun supervisedScopeException() = runBlocking {
+        val handler = CoroutineExceptionHandler { _, exception ->
+            println("Caught $exception")
+        }
+        supervisorScope {
+            val child = launch(handler) {
+                println("Child throws an exception")
+                throw AssertionError()
+            }
+            println("Scope is completing")
+        }
+        println("Scope is completed")
     }
 }
