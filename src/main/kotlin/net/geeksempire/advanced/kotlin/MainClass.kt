6fad8bdab3b6a450f113892f45a9a9c770301991
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-10-23 09:06:02 -0800

    Initial

--- src/main/kotlin/net/geeksempire/advanced/kotlin/MainClass.kt
+++ src/main/kotlin/net/geeksempire/advanced/kotlin/MainClass.kt
@@ -3,15 +3,46 @@ package net.geeksempire.advanced.kotlin
 import kotlinx.coroutines.*
 
 fun main(/*args: Array<String>*/) = runBlocking<Unit> {
-    execute(operations = Operations.PressHold(NumberDataClass(aNumber = 13)))
 
-    execute(operations = Operations.Press(NumberDataClass(aNumber = 13)))
+    val aArray = arrayListOf<Int>()
+
+    repeat(52) {
+        aArray.add(it)
+    }
+    println(aArray)
+
+    aArray.shuffle()
+    println(aArray)
+
+    println("\n")
+
+    val a = aArray.slice(IntRange(0, 12))
+    val b = aArray.slice(IntRange(13, 25))
+    val c = aArray.slice(IntRange(26, 38))
+    val d = aArray.slice(IntRange(39, 51))
+
+    println(a)
+    println(a.size)
+
+    println(b)
+    println(b.size)
+
+    println(c)
+    println(c.size)
+
+    println(d)
+    println(d.size)
 
 
-
-    execute(operations = Operations.PressHoldAction)
-
-    execute(operations = Operations.PressAction)
+//    execute(operations = Operations.PressHold(NumberDataClass(aNumber = 13)))
+//
+//    execute(operations = Operations.Press(NumberDataClass(aNumber = 13)))
+//
+//
+//
+//    execute(operations = Operations.PressHoldAction)
+//
+//    execute(operations = Operations.PressAction)
 }
 
 fun execute(operations: Operations) = when (operations) {
